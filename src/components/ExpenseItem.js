51558da86ab80f16dddc0b725dfacc2d5bc89af5
Mikhail Petushok <72050332+Mike10581@users.noreply.github.com>
2023-06-01 10:47:17 -0400

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
 
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
     
     const handleDeleteExpense = () => {
         dispatch({
@@ -24,11 +24,41 @@ const ExpenseItem = (props) => {
         });
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>{currency}{props.cost}</td>
+        <td><button style={{
+            backgroundColor: 'lightgreen', 
+            color: 'white',
+            fontWeight: 'bold',            
+            width: '30px',
+            height: '30px',
+            borderRadius: '50%',
+            border: 'none',
+            padding: '0'
+            }} onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button style={{
+            backgroundColor: 'red', 
+            color: 'white',
+            fontWeight: 'bold',            
+            width: '30px',
+            height: '30px',
+            borderRadius: '50%',
+            border: 'none',
+            padding: '0'
+            }}onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
